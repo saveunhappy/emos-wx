de1312a1299fc7501517cd9f8ba3f30806fbcece
JiangTaoHou <925147954@qq.com>
2022-08-16 21:28:05 +0800

    init

---
+++ pages/demo/demo.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view>{{username}}</view>
+		<view v-for="one in tel">{{one}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username:"Scott",
+				tel:['12341231231','123134234234']
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
